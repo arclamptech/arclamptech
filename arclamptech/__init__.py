014ace5f9470ddbefd18fc02e3879cc2cc9f60eb
azraelgnosis <azraelgnosis@gmail.com>
2020-03-24 03:09:30 -0400

    Add arclamp blueprint

--- arclamptech/__init__.py
+++ arclamptech/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
 
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import arclamptech
+    app.register_blueprint(arclamptech.bp)
 
     return app
