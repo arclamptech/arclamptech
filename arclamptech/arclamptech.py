edc4f66408f67d97153fe0bec2aba23ab8037a72
azraelgnosis <azraelgnosis@gmail.com>
2020-03-28 00:19:12 -0400

    Add IP Address display view

--- arclamptech/arclamptech.py
+++ arclamptech/arclamptech.py
@@ -1,9 +1,18 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request
 )
+from flask import jsonify
 
 bp = Blueprint("arclamp", __name__)
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/resources/')
+def resources():
+    return render_template('resources.html')
+
+@bp.route('/reveal')
+def reval():
+    return jsonify({'ip': request.remote_addr}), 200
